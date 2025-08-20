5f5bbfd0cd30cd059f8baa367cb4b79023b5d93a
Terje K Wahl <terje.k.wahl@gmail.com>
2025-08-20 13:35:02 +0200

    Clean up. Added reading controller trigger and converting it to desired gripper angle. Added isRecording true/false, controlled by A button.

---
+++ VR/RobotArmRemoteController/Assets/Scripts/Configuration.cs
@@ -0,0 +1,7 @@
+public class Configuration
+{
+    public const int FRAMES_SEND_MESSAGE_PERIOD = 2; // How often to send UDP messages to the PC, related 
+                                                     // to framerate (and execution of update() function)
+    public const string PC_IP_ADDRESS = "192.168.0.43";
+    public const int PC_PORT_NUMBER = 7506;
+}
--- VR/RobotArmRemoteController/Assets/Scripts/UITextUpdater.cs
+++ VR/RobotArmRemoteController/Assets/Scripts/UITextUpdater.cs
@@ -5,29 +5,106 @@ public class UITextUpdater : MonoBehaviour
 {
     public GameObject RobotGripper;
     private TMP_Text textMeshPro;
-    private const int FRAMES_SEND_MESSAGE_PERIOD = 2; // TODO: Move to config file
-    private const string PC_IP_ADDRESS = "192.168.0.43"; // TODO: Move to config file
-    private const int PC_PORT_NUMBER = 7506; // TODO: Move to config file
-    private readonly UdpManager udpManager = new UdpManager(PC_IP_ADDRESS, PC_PORT_NUMBER); 
+    private readonly UdpManager udpManager = new UdpManager(Configuration.PC_IP_ADDRESS, Configuration.PC_PORT_NUMBER);
     private RobotMessageManager robotMessageManager = new RobotMessageManager();
     private Matrix4x4 startPositionMatrix = Matrix4x4.zero;
-    // Definition of transformation matrix for conversion from 
-    // Unity's coordinate system (x-right, y-up, z-forward)
-    // to the robot arm's coordinate system (x-forward, y-left, z-up):
-    /*private Matrix4x4 translationAndRotationMatrix = new Matrix4x4(
-        new Vector4(0, -1, 0, 0),  // Column 0
-        new Vector4(0, 0, 1, 0),   // Column 1
-        new Vector4(1, 0, 0, 0),   // Column 2
-        new Vector4(0, 0, 0, 1)    // Column 3, including translation which is 0 here because 
-                                   // it will be saved here later on startup
-    );*/
     private Vector3 startPosition = Vector3.zero;
     private Vector3 startRotation = Vector3.zero;
+    private bool isRecording = false; // Used to track status of recording on/off, for signaling to PC when to start/stop recording training videoes
+    private bool isAPressedLast = false; // To track change in A button state
 
     void Start()
     {
         textMeshPro = GetComponent<TMP_Text>();
     }
+
+
+    /// <summary>
+    /// Update the text with the current position and rotation of the RobotGripper,
+    /// as an offset from the starting position and rotation, in the coordinate system of the
+    /// robot arm (not Unity's coordinate system).
+    /// </summary>
+    void Update()
+    {
+        if (RobotGripper != null)
+        {
+            // Initialize start position and rotation matrices if they are not set yet
+            if (startPositionMatrix == Matrix4x4.zero)
+            {
+                startPositionMatrix = RobotGripper.transform.localToWorldMatrix;
+                Debug.Log($"Start position matrix initialized to \n{startPositionMatrix}.");
+                //startPosition = RobotGripper.transform.position;
+                //translationAndRotationMatrix.SetColumn(3,
+                //    new Vector4(-startPosition.x, -startPosition.y, -startPosition.z, 1));
+                //Debug.Log($"Translation and rotation matrix updated to \n{translationAndRotationMatrix}.");
+            }
+            if (startPosition == Vector3.zero)
+            {
+                startPosition = RobotGripper.transform.position;
+            }
+            if (startRotation == Vector3.zero)
+            {
+                startRotation = RobotGripper.transform.eulerAngles;
+            }
+
+            // Get the current position and rotation of the RobotGripper
+            Matrix4x4 positionMatrix = RobotGripper.transform.localToWorldMatrix;
+
+            Vector3 position = RobotGripper.transform.position;
+            Vector3 rotation = RobotGripper.transform.eulerAngles;
+            // Convert to mm, and note the x,y,z axis changes because of different coordinate system for robot arm:
+            float posX = (position.z - startPosition.z) * 1000;
+            float posY = (position.x - startPosition.x) * -1000;
+            float posZ = (position.y - startPosition.y) * 1000;
+            float rotX = rotation.x - startRotation.x;
+            float rotY = rotation.z - startRotation.z;
+            float rotZ = rotation.y - startRotation.y;
+            if (rotX > 180) rotX -= 360; // Want to keep the range of rotation values between -180 and 180 degrees
+            if (rotY > 180) rotY -= 360;
+            if (rotZ > 180) rotZ -= 360;
+
+            // Get current VR controller right index trigger button value:
+            float rightTriggerValue = OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger);
+
+            bool isAPressed = OVRInput.Get(OVRInput.Button.One); // True if A button on right controller is currently pressed
+            if (isAPressed && !isAPressedLast)
+            {
+                isRecording = !isRecording;
+                Debug.Log($"Recording status changed: {isRecording}");
+            }
+            isAPressedLast = isAPressed;
+
+
+            int gripperAngle = 45 - (int)(rightTriggerValue * 45);
+
+            // Format position and rotation strings
+            string posXStr = FormatNumber(posX);
+            string posYStr = FormatNumber(posY);
+            string posZStr = FormatNumber(posZ);
+            string rotXStr = FormatNumber(rotX);
+            string rotYStr = FormatNumber(rotY);
+            string rotZStr = FormatNumber(rotZ);
+
+            string text = $"Position (mm):  (X, Y, Z): {posXStr} {posYStr} {posZStr}\n" +
+                           $"Orientation (°)   (X, Y, Z): {rotXStr} {rotYStr} {rotZStr}\n" +
+                           $"(X is forward, Y is to the left, Z is up)\n" +
+                           $"Gripper angle (0 is closed): {gripperAngle}°\n" +
+                           $"Recording: {(isRecording ? "On" : "Off")}\n" +
+                           $"Current pos matrix: \n{positionMatrix}\n"
+                           //$"Gripper (0-100% pressed): {(int)(rightTriggerValue * 100)}%\n" +
+                           //$"A button pressed: {isAPressed}\n"
+                           ;
+            textMeshPro.text = text;
+
+            // Send UDP packet, but only every FRAMES_SEND_MESSAGE_PERIOD frames to avoid flooding the network
+            if (Time.frameCount % Configuration.FRAMES_SEND_MESSAGE_PERIOD == 0)
+            {
+                byte[] messageBytes = robotMessageManager.CreateMessageFromVRToPC(positionMatrix);
+                udpManager.SendMessage(messageBytes);
+            }
+        }
+    }
+
 
     /// <summary>
     /// Create text string with whole numbers from a float. 
@@ -50,82 +127,4 @@ public class UITextUpdater : MonoBehaviour
             return $"{sign}{absValue}";
     }
 
-    /// <summary>
-    /// Update the text with the current position and rotation of the RobotGripper,
-    /// as an offset from the starting position and rotation, in the coordinate system of the
-    /// robot arm (not Unity's coordinate system).
-    /// </summary>
-    void Update()
-    {
-        if (RobotGripper != null)
-        {
-            if (startPositionMatrix == Matrix4x4.zero)
-            {
-                startPositionMatrix = RobotGripper.transform.localToWorldMatrix;
-                Debug.Log($"Start position matrix initialized to \n{startPositionMatrix}.");
-                //startPosition = RobotGripper.transform.position;
-                //translationAndRotationMatrix.SetColumn(3,
-                //    new Vector4(-startPosition.x, -startPosition.y, -startPosition.z, 1));
-                //Debug.Log($"Translation and rotation matrix updated to \n{translationAndRotationMatrix}.");
-            }
-            if (startPosition == Vector3.zero)
-            {
-                startPosition = RobotGripper.transform.position;
-            }
-            if (startRotation == Vector3.zero)
-            {
-                startRotation = RobotGripper.transform.eulerAngles;
-            }
-
-            Matrix4x4 positionMatrix = RobotGripper.transform.localToWorldMatrix;
-            //Matrix4x4 offsetMatrix = positionMatrix * startPositionMatrix.inverse;
-            //Matrix4x4 convertedMatrix = positionMatrix * translationAndRotationMatrix;
-
-            Vector3 position = RobotGripper.transform.position;
-            Vector3 rotation = RobotGripper.transform.eulerAngles;
-            // Convert to mm, and note the x,y,z axis changes because of different coordinate system for robot arm:
-            float posX = (position.z - startPosition.z) * 1000;
-            float posY = (position.x - startPosition.x) * -1000;
-            float posZ = (position.y - startPosition.y) * 1000;
-            float rotX = rotation.x - startRotation.x;
-            float rotY = rotation.z - startRotation.z;
-            float rotZ = rotation.y - startRotation.y;
-            if (rotX > 180) rotX -= 360; // Want to keep the range of rotation values between -180 and 180 degrees
-            if (rotY > 180) rotY -= 360;
-            if (rotZ > 180) rotZ -= 360;
-
-            // Format position and rotation strings
-            string posXStr = FormatNumber(posX);
-            string posYStr = FormatNumber(posY);
-            string posZStr = FormatNumber(posZ);
-            string rotXStr = FormatNumber(rotX);
-            string rotYStr = FormatNumber(rotY);
-            string rotZStr = FormatNumber(rotZ);
-
-            string text = $"Position (mm):  (X, Y, Z): {posXStr} {posYStr} {posZStr}\n" +
-                           $"Orientation (°)   (X, Y, Z): {rotXStr} {rotYStr} {rotZStr}\n" +
-                           $"X is forward\n" +
-                           $"Y is to the left\n" +
-                           $"Z is up\n" +
-                           //$"Start pos matrix: \n{startPositionMatrix}.\n" +
-                           //$"translationAndRotationMatrix: \n{translationAndRotationMatrix}.\n" +
-                           $"Current pos matrix: \n{positionMatrix}.\n"
-                           //$"Offset matrix: \n{offsetMatrix}.\n" +
-                           //$"Converted matrix: \n{convertedMatrix}.\n" +
-                           //$"RobotGripper.transform.localScale: {RobotGripper.transform.localScale}.\n"
-                           //$"RobotGripper.transform.localToWorldMatrix: \n{RobotGripper.transform.localToWorldMatrix}.\n" +
-                           //$"Connection status:\n" +
-                           //$"PC: 	Not connected\n" + // TODO: Implement actual connection status
-                           //$"Controllers:	Not connected\n" +
-                           ;
-            textMeshPro.text = text;
-            
-            // Send UDP packet, but only every FRAMES_SEND_MESSAGE_PERIOD frames to avoid flooding the network
-            if (Time.frameCount % FRAMES_SEND_MESSAGE_PERIOD == 0)
-            {
-                byte[] messageBytes = robotMessageManager.CreateMessageFromVRToPC(positionMatrix);
-                udpManager.SendMessage(messageBytes);
-            }
-        }
-    }
 }
